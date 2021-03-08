490a32490a1348dad155dac2f451e1b1d9b6c5db
Remi Zeltser <remi.zeltser@epitech.eu>
2021-03-08 16:25:31 +0100

    [MODIFIED] sayHello.ts

--- part1/sayHello.ts
+++ part1/sayHello.ts
@@ -2,5 +2,5 @@ function sayHello(name?: string) {
     if (name != undefined)
         console.log(`Hello ${name}`);
     else 
-        console.log('Hello World');
+        console.log('Hello world');
 }
